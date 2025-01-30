f418f9f0668670eda15dd6617fbd7bb632b92502
mmustar <mmustar@free.fr>
2025-01-30 13:56:07 +0100

    config nginx en cours

--- ansible/roles/common/files/get_secret.py
+++ ansible/roles/common/files/get_secret.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import boto3
 import json
+import sys
 
-client = boto3.client('secretsmanager', region_name='eu-west-3')
-response = client.get_secret_value(SecretId='book')
-print(response['SecretString'])
+try:
+    client = boto3.client('secretsmanager', region_name='eu-west-3')
+    response = client.get_secret_value(SecretId='book')
+    print(response['SecretString'])
+except Exception as e:
+    print(f"Erreur: {str(e)}", file=sys.stderr)
+    sys.exit(1)
